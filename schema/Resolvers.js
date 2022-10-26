b142e4fcb45a22ec2f34d28a80895fa13fee973a
Sailor Metz <sailormetz@gmail.com>
2022-10-25 22:55:12 -0500

    todo operations

--- schema/Resolvers.js
+++ schema/Resolvers.js
@@ -1,39 +1,109 @@
+import { GraphQLInt8 } from 'graphql-type-ints';
 import { prisma } from '../lib/db.js';
+
+// * queries
+const getAllUsers = async () => {
+  return prisma.user.findMany();
+};
+
+const getUser = async (_, args) => {
+  return prisma.user.findUnique({
+    where: {
+      email: args.email,
+    },
+  });
+};
+
+const getAllTodos = async () => {
+  return prisma.todo.findMany();
+};
+
+const getTodo = async (_, args) => {
+  return prisma.todo.findUnique({
+    where: {
+      id: args.id,
+    },
+  });
+};
+
+// * mutations
+const createUser = async (_, args) => {
+  return prisma.user.create({
+    data: args,
+  });
+};
+
+const editUser = async (_, args) => {
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email: args.email,
+    },
+  });
+  if (!userExists) {
+    throw new Error('not found');
+  }
+  return prisma.user.update({
+    where: {
+      email: args.email,
+    },
+    data: args,
+  });
+};
+
+const createTodo = async (_, args) => {
+  return prisma.todo.create({
+    data: args,
+  });
+};
+
+const editTodo = async (_, args) => {
+  const todoExists = await prisma.todo.findUnique({
+    where: {
+      id: args.id,
+    },
+  });
+  if (!todoExists) {
+    throw new Error('not found');
+  }
+  return prisma.todo.update({
+    where: {
+      id: args.id,
+    },
+    data: args,
+  });
+};
+
+const deleteTodo = async (_, args) => {
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id: args.id,
+    },
+  });
+  if (!todo) {
+    throw new Error('not found');
+  }
+  return prisma.todo.delete({
+    where: {
+      id: args.id,
+    },
+  });
+};
+
 export const resolvers = {
+  GraphQLInt8,
   Query: {
-    getAllUsers() {
-      return prisma.user.findMany();
-    },
-    getUser(parent, args) {
-      return prisma.user.findUnique({
-        where: {
-          email: args.email,
-        },
-      });
-    },
-    getAllTodos() {
-      return prisma.todo.findMany();
-    },
-    getTodo(parent, args) {
-      return prisma.todo.findUnique({
-        where: {
-          id: args.id,
-        },
-      });
-    },
+    getAllUsers,
+    getUser,
+    getAllTodos,
+    getTodo,
   },
 
   // parent, arge are other arguments that get passes to resolvers automatically
   Mutation: {
-    createUser(parent, args) {
-      return prisma.user.create({
-        data: args,
-      });
-    },
-    createTodo(parent, args) {
-      return prisma.todo.create({
-        data: args,
-      });
-    },
+    createUser,
+    editUser,
+    createTodo,
+    editTodo,
+    deleteTodo,
   },
 };
--- schema/TypeDefs.js
+++ schema/TypeDefs.js
@@ -1,5 +1,12 @@
-export const typeDefs = `
+/* eslint-disable no-unused-vars */
+import { GraphQLInt8 } from 'graphql-type-ints';
+
+export const typeDefs = `#graphql
+
+  scalar GraphQLInt8
+
   type User {
+    id: GraphQLInt8!
     name: String!
     email: String!
     age: Int!
@@ -7,21 +14,31 @@ export const typeDefs = `
   }
 
   type Todo {
+    id: GraphQLInt8!
     title: String!
     description: String!
+    due_date: String
   }
 
   type Query {
     # users queries
     getAllUsers: [User]
     getUser(email: String!): User
+
     # todo queries
     getAllTodos: [Todo]
-    getTodo(id: String!): Todo
+    getTodo(id: Int!): Todo
   }
 
   type Mutation {
+    # user
     createUser(name: String!, email: String!, age: Int!): User
-    createTodo(title: String!, description: String!): Todo
+    editUser(name: String, email: String!, age: Int): User
+    deleteUser(email: String!): User
+
+    # todo
+    createTodo(title: String!, description: String!, due_date: String): Todo
+    editTodo(id: Int, title: String, description: String): Todo
+    deleteTodo(id: Int!): Todo
   }
 `;
